880493ee3095dce5c20a2dc32b767e6ce8b309a1
Faris Aziz <farisaziz12@gmail.com>
2020-04-11 22:36:57 +0200

    percentages and amount increase and decrease showing with colors added. Dates added but need to be fixed for following day

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Card from './Components/Card';
 
@@ -9,18 +8,27 @@ class App extends React.Component {
   }
 
   componentDidMount(){
-    fetch("http://localhost:3000/data").then(resp => resp.json()).then(data => this.setState({data: Object.entries(data)}))
+    fetch("http://localhost:3000/data").then(resp => resp.json()).then(data => this.setState({data}))
   }
 
   render() {
     const { data } = this.state
+    const todayForcastsWithData = data.filter(po => po[1].today[0] !== undefined )
+    const tomorrowForcastsWithData = data.filter(po => po[1].tomorrow[0] !== undefined )
     return (
 
       <>
       <h1 className='title'>NTS Linepack</h1>
-      {data.map(po => (
-        <Card po={po}/>
-      ))}
+      <div className='today-cards-container'>
+        {todayForcastsWithData.map(po => (
+          <Card po={po} today={true}/>
+        ))}
+      </div>
+      <div className='tomorrow-cards-container'>
+        {tomorrowForcastsWithData.map(po => (
+          <Card po={po} tomorrow={true}/>
+        ))}
+      </div>
       </>
     );
   }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,18 +8,41 @@ export default class Card extends Component {
     }
 
     componentDidMount() {
-        const data = this.props.po[1].sort((a,b) => new Date (b.at) - new Date (a.at))
-        console.log(data)
-        this.setState({data})
+        const { today, tomorrow } = this.props
+
+        if (today) {
+            const data = this.props.po[1].today.sort((a,b) => new Date (b.at) - new Date (a.at))
+            this.setState({data})
+        } else if (tomorrow) {
+            const data = this.props.po[1].tomorrow.sort((a,b) => new Date (b.at) - new Date (a.at))
+            this.setState({data})
+        }
+        
     }
 
     render() {
-        const { po } = this.props
+        // tomorrow? new Date(data[0].date) : new Date(data[0].at)
+        const { po, tomorrow } = this.props
         const { data } = this.state
+        const date = data[0]&& new Date(data[0].at)
+        const dayAndMonth = date&& date.toString().slice(4, 10)
+        const time = date&& date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        const value = data[0]&& data[0].value
+        const prevValue = data[1]&& data[1].value
+        const valueDifference = value - prevValue
+        const percentageChange = value&& prevValue&& value === prevValue? 0 : (value - prevValue) / prevValue
+        const incOrDecCheck = valueDifference === 0? "white" : value > prevValue? "#4EAD51" : "#DD481D"
+        const inc = value > prevValue? true : false
+        const checkPercentageChange = percentageChange&& percentageChange.toFixed(1) === "0.0"|| percentageChange.toFixed(1) === "-0.0" || percentageChange === 0? "0.0" : percentageChange
+        const formatedPercentageChange = checkPercentageChange === "0.0"? checkPercentageChange : checkPercentageChange.toFixed(1)
+        const formatedValueDifference = valueDifference&& valueDifference === 0? "0.00" : valueDifference.toFixed(2)
         return (
-            <div className='card'>
+            <div className='card' style={tomorrow? {backgroundColor: "#222222"} : {backgroundColor: "#283838"}}>
                 <h2 className='card-title'>{po[0]}</h2>
-                <h4 className='txt'>{data[0]&& data[0].value.toFixed(2)}</h4>
+                <h3 className='date'>{dayAndMonth + " " + time}</h3>
+                <h2 className='value'>{value&& value.toFixed(2)}</h2>
+            <p className='value-diff' style={{color: incOrDecCheck}}>{inc&&"+"}{formatedValueDifference&& formatedValueDifference}</p>
+            <p className='value-diff' style={{color: incOrDecCheck}}>{inc&&"+"}{formatedPercentageChange&& formatedPercentageChange} %</p>
             </div>
         )
     }
